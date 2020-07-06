afc2dc43194118e40178ff2eef281c0ec8dd5abb
Jappreet Singh <jappreet.singh@steadybiz.in>
2020-07-06 12:38:38 +0530

    removed ProtectedRoute

--- src/client/containers/index.js
+++ src/client/containers/index.js
@@ -3,7 +3,6 @@ import { BrowserRouter, Route, Switch } from "react-router-dom";
 
 import COMMON_ROUTES from "./../constants/routes/routes.common";
 
-import ProtectedRoute from "./ProtectedRoute";
 import Home from "./../views/home";
 
 const App = () => {
@@ -15,7 +14,6 @@ const App = () => {
         <Route exact path={COMMON_ROUTES.home} component={Home} />
         <Route exact path={COMMON_ROUTES.login} component={() => <div>Login</div>} />
         <Route exact path={COMMON_ROUTES.signUp} component={() => <div>Sign Up</div>} />
-        <ProtectedRoute exact path={COMMON_ROUTES.search} component={() => <div>Content based on Route goes here</div>} />
         <Route path="*" component={() => "404 NOT FOUND"} />
       </Switch>
     </BrowserRouter>
