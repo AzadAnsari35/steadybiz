6068dc9a7568c7eb6f83f85d956e05d0159d26cc
himanshu-kumar-steadybiz <himanshu.kumar@steadybiz.in>
2020-07-17 16:51:34 -0700

    bind dropDown using API

--- src/client/components/Offices/Office/OfficeRegistration/index.js
+++ src/client/components/Offices/Office/OfficeRegistration/index.js
@@ -14,6 +14,7 @@ import { useDispatch, useSelector } from 'react-redux';
 import { regex } from 'Helpers/validator';
 import useAsyncEndpoint from 'Hooks/useAsyncEndpoint';
 import useDropDown from 'Hooks/useDropDown';
+import useDropDownApi from 'Hooks/useDropDownApi';
 // import useAPi from 'Hooks/useApi';
 // import endpoint from 'Config/endpoint';
 import './style.scss';
@@ -35,7 +36,7 @@ const OfficeRegistrationForm = () => {
     watch,
   } = useForm();
   const [regResponse, postRegResponse] = regEndpoint();
-
+  const countryDropDownApiList = useDropDownApi(endpoint.master.countries);
   const countryDropDownList = useDropDown(
     endpoint.master.countries,
     'masterCountries'
@@ -116,10 +117,7 @@ const OfficeRegistrationForm = () => {
             <SelectWithTextInput
               name="mobile"
               selectInputName="mobileDialCode"
-              data={[
-                { label: 'India', value: 'IN (+91)' },
-                { label: 'Canada', value: 'CA (+1)' },
-              ]}
+              data={countryDropDownApiList.dropDownItems}
               label="Mobile Number"
               placeholder="Mobile Number"
               selectPlaceholder="Dial Code"
