3d38e4bb27a7e28fbce9139d13e85e82dd938a7d
himanshu-kumar-steadybiz <himanshu.kumar@steadybiz.in>
2020-07-17 12:11:21 -0700

    dropdown redux

--- src/config/endpoint.js
+++ src/config/endpoint.js
@@ -1,22 +1,31 @@
-import config from "./index";
-import { actionTypeConstant, commonConstant } from "Constants/";
+import config from './index';
+import { actionTypeConstant, commonConstant } from 'Constants/';
 export default {
   flights: {
     flightRevalidate: {
-      url: config.api.url + "/flights/flightRevalidate",
+      url: config.api.url + '/flights/flightRevalidate',
       isAuth: true,
     },
     flightSelect: {
-      url: config.api.url + "/flights/flightSelect",
+      url: config.api.url + '/flights/flightSelect',
       isAuth: true,
     },
   },
   user: {
     login: {
-      url: config.api.url + "/user/login",
+      url: config.api.url + '/api/v1/user/login',
       isAuth: false,
       httpVerb: commonConstant.HttpVerbsConstant.POST,
       actionType: actionTypeConstant.users.signIn,
+    },
+  },
+  master: {
+    countries: {
+      url: config.api.url + '/api/v1/master/countries',
+      isAuth: true,
+      httpVerb: commonConstant.HttpVerbsConstant.GET,
+      actionType: actionTypeConstant.master.countries,
+      dropDownParam: { label: 'countryname', value: 'countryCode' },
     },
   },
 };
