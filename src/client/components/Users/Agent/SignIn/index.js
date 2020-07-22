cd1befbffaf976e44ee724b7348015b70ea5a1ba
himanshu-kumar-steadybiz <himanshu.kumar@steadybiz.in>
2020-07-21 18:06:09 -0700

    impment session storage

--- src/client/components/Users/Agent/SignIn/index.js
+++ src/client/components/Users/Agent/SignIn/index.js
@@ -2,13 +2,15 @@ import React, { useState, useEffect } from 'react';
 import { useForm } from 'react-hook-form';
 import { TextInput, Button, Toast, PrimaryLoader } from 'Widgets';
 import { regex } from 'Helpers/validator';
-import { showError } from 'Helpers/utils';
+import { showError} from 'Helpers/utils';
 import { useDispatch, useSelector } from 'react-redux';
 import commonAction from 'Actions/';
 import endpoint from 'Config/endpoint';
 import { utils } from 'Helpers/index';
+import { useHistory } from 'react-router-dom';
 
 const SignInForm = () => {
+  const history=useHistory();
   const { register, handleSubmit, watch, errors } = useForm();
   const [error, setError] = useState(null);
   const [errorMsg, setErrorMsg] = useState('');
@@ -22,8 +24,21 @@ const SignInForm = () => {
   const postLogin = () => {
     if (apiResponse.items != null) {
       const errMsg = utils.checkError(apiResponse);
+      if(errMsg!=='')
       setErrorMsg(errMsg);
+      else
+      {
+        const responseJson=apiResponse.items.data;
+        utils.setItemToStorage('userToken',responseJson.token);
+        utils.setItemToStorage('userId',responseJson.userDto.userId);
+        utils.setItemToStorage('officeId',responseJson.userDto.officeId);
+        history.push('/Office/Registration');        
+
+      }
+      
+      
     }
+    
     //alert(apiResponse.items.success);
   };
 
--- src/client/helpers/utils.js
+++ src/client/helpers/utils.js
@@ -1,8 +1,9 @@
 import config from 'Config/';
-import endpoint from 'Config/endpoint';
+
 export const displayImage = (name, path = 'images') => {
   return '/public/assets/' + path + '/' + name;
 };
+
 export const isLogin = () => {
   return true;
 };
@@ -12,13 +13,29 @@ export const isDevelopment = () => {
 export const showError = (exception, errorMessage) => {
   errorMessage(exception.message);
 };
-export const getItemFromLocalStorage = (key) => {
-  return localStorage.getItem(key);
+export const setItemToStorage=(key,value,storageType=1) =>{
+  if(storageType===1)
+  sessionStorage.setItem(key,value);
+  else
+  localStorage.setItem(key,value);
+}
+export const getItemFromStorage = (key,storageType=1) => {
+  let item='';
+  if(storageType===1)
+  {
+    item=sessionStorage.getItem(key);    
+  }
+  else
+  {
+    item=sessionStorage.getItem(key);  
+  }
+     
+      return item;
 };
 export const appendHeader = (endpoint) => {
   //const token =getItemFromLocalStorage('userToken');
-  const token =
-    'eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJ7XCJ1c2VySWRcIjpcIjc5NGQ2ZDAzLTM2N2EtNDk1OS04NDBlLTY0Mzk0NzcxMjM1OFwiLFwib2ZJZFwiOlwiOWNhZTU0YzYtYmI1Zi00NWEzLWEzNTQtZDAxOGVjN2E0NDk4XCIsXCJyb290T2ZJZFwiOlwiOWNhZTU0YzYtYmI1Zi00NWEzLWEzNTQtZDAxOGVjN2E0NDk4XCIsXCJwYXJlbnRPZklkXCI6XCI5Y2FlNTRjNi1iYjVmLTQ1YTMtYTM1NC1kMDE4ZWM3YTQ0OThcIixcImlzTWFzdGVyXCI6dHJ1ZSxcImZ1bmN0aW9uR3JvdXBzXCI6W119IiwiZXhwIjoxNTk0OTcxNjQwLCJpYXQiOjE1OTQ4ODUyNDB9.jLxH_aX-2Q3bZn_qXnNaJfWymtluYlm48Wu7_wk1VTlqCZr8Gs78lRzLFHRDyiCTdx19ewR-hmXW7BnEcQy6FQ';
+  const token =getItemFromStorage('userToken');
+    //'eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJ7XCJ1c2VySWRcIjpcIjc5NGQ2ZDAzLTM2N2EtNDk1OS04NDBlLTY0Mzk0NzcxMjM1OFwiLFwib2ZJZFwiOlwiOWNhZTU0YzYtYmI1Zi00NWEzLWEzNTQtZDAxOGVjN2E0NDk4XCIsXCJyb290T2ZJZFwiOlwiOWNhZTU0YzYtYmI1Zi00NWEzLWEzNTQtZDAxOGVjN2E0NDk4XCIsXCJwYXJlbnRPZklkXCI6XCI5Y2FlNTRjNi1iYjVmLTQ1YTMtYTM1NC1kMDE4ZWM3YTQ0OThcIixcImlzTWFzdGVyXCI6dHJ1ZSxcImZ1bmN0aW9uR3JvdXBzXCI6W119IiwiZXhwIjoxNTk0OTcxNjQwLCJpYXQiOjE1OTQ4ODUyNDB9.jLxH_aX-2Q3bZn_qXnNaJfWymtluYlm48Wu7_wk1VTlqCZr8Gs78lRzLFHRDyiCTdx19ewR-hmXW7BnEcQy6FQ';
   return {
     'Content-Type': 'application/json',
     ...(endpoint.isAuth && { Authorization: `Bearer ${token}` }),
