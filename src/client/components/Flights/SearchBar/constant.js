daf51f408fa3921fdeb7aa809cc4c52d55ae5db9
JappreetS-Steadybiz <64848958+JappreetS-Steadybiz@users.noreply.github.com>
2020-09-03 11:39:24 +0530

    search bar bug fixes

---
+++ src/client/components/Flights/SearchBar/constant.js
@@ -0,0 +1,5 @@
+export const ERROR_MESSAGE = {
+	DEPARTURE_AIRPORT_REQUIRED: "Please select Departure Airport",
+	ARRIVAL_AIRPORT_REQUIRED: "Please select Arrival Airport",
+	DEPARTURE_ARRIVAL_SAME: "Departure and Arrival Airports cannot be same",
+};
