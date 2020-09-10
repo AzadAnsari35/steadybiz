ad386f971a80eddd6d3e9f4501caa3e57183d8ce
JappreetS-Steadybiz <64848958+JappreetS-Steadybiz@users.noreply.github.com>
2020-09-10 15:18:50 +0530

    home page responsive ui updated

--- src/client/components/Home/Carousel/index.js
+++ src/client/components/Home/Carousel/index.js
@@ -7,6 +7,7 @@ import { utils } from 'Helpers';
 import SignInForm from 'Components/Users/Agent/SignIn';
 import ForgotPasswordForm from 'Components/Users/Agent/ForgotPassword';
 import ResetPasswordForm from 'Components/Users/Agent/ResetPassword';
+import { Text } from 'Widgets/';
 
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
@@ -32,26 +33,40 @@ const Carousel = () => {
   };
 
   return (
-    <div className="Carousel">
+    <div className="Carousel position-relative">
       <Slider {...settings}>
-        <div className="Carousel-getStarted"></div>
+        <div className="Carousel-getStarted" />
       </Slider>
-      <div
-        className={`Carousel-form ${
-          selectedForm === 'SignInForm' ? 'top-115' : 'top-154'
-        } ${isResetPassword ? 'top-148' : ''}`}
-      >
-        <div className="Carousel-getStarted-right">
-          {isResetPassword ? (
-            <ResetPasswordForm />
-          ) : selectedForm === 'SignInForm' ? (
-            <SignInForm setSelectedForm={setSelectedForm} />
-          ) : (
-            <ForgotPasswordForm
-              setSelectedForm={setSelectedForm}
-              selectedForm={selectedForm}
+      <div className="Carousel-content layout-wrapper">
+        <div className="d-flex justify-content-between align-items-center height-100">
+          <div className="Carousel-content__left">
+            <Text
+              className="title font-primary-bold-26"
+              text="Leading B2B travel portal"
             />
-          )}
+            <Text
+              className="description"
+              text="OK Travel & Tourism is UAE's largest B2B travel portal. OK Travel is providing Global B2B booking system powering global travel solution to travel partners to serve their customers efficiently, with the right pricing and inventory."
+            />
+          </div>
+          <div
+            className={`Carousel-form ${
+              selectedForm === 'SignInForm' ? 'top-115' : 'top-154'
+            } ${isResetPassword ? 'top-148' : ''}`}
+          >
+            <div className="Carousel-getStarted-right">
+              {isResetPassword ? (
+                <ResetPasswordForm />
+              ) : selectedForm === 'SignInForm' ? (
+                <SignInForm setSelectedForm={setSelectedForm} />
+              ) : (
+                <ForgotPasswordForm
+                  setSelectedForm={setSelectedForm}
+                  selectedForm={selectedForm}
+                />
+              )}
+            </div>
+          </div>
         </div>
       </div>
     </div>
